ddb2fe88c9ce0c7649153d480d3e40f05423185f
dipanshuhappy <dipanshuhappy@gmail.com>
2023-06-26 18:55:17 +0530

    Initial commit from create-refine-app

---
+++ pages/blog-posts/create/index.tsx
@@ -0,0 +1,43 @@
+import { ChakraUICreateInferencer } from "@refinedev/inferencer/chakra-ui";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { authProvider } from "src/authProvider";
+
+export default function BlogPostCreate() {
+  return (
+    <ChakraUICreateInferencer
+      fieldTransformer={(field) => {
+        if (["$permissions", "$updatedAt", "$createdAt"].includes(field.key)) {
+          return false;
+        }
+        return field;
+      }}
+    />
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const { authenticated, redirectTo } = await authProvider.check(context);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  if (!authenticated) {
+    return {
+      props: {
+        ...translateProps,
+      },
+      redirect: {
+        destination: `${redirectTo}?to=${encodeURIComponent("/blog-posts")}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
---
+++ pages/blog-posts/show/[id].tsx
@@ -0,0 +1,43 @@
+import { ChakraUIShowInferencer } from "@refinedev/inferencer/chakra-ui";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { authProvider } from "src/authProvider";
+
+export default function BlogPostShow() {
+  return (
+    <ChakraUIShowInferencer
+      fieldTransformer={(field) => {
+        if (["$permissions", "$updatedAt", "$createdAt"].includes(field.key)) {
+          return false;
+        }
+        return field;
+      }}
+    />
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const { authenticated, redirectTo } = await authProvider.check(context);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  if (!authenticated) {
+    return {
+      props: {
+        ...translateProps,
+      },
+      redirect: {
+        destination: `${redirectTo}?to=${encodeURIComponent("/blog-posts")}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
